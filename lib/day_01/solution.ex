a24d4c428b7f6c3956af1aa86481599e51ca9a40
steve.brazier <steve.b@lendinvest.com>
2016-12-02 16:27:26 +0000

    rotating position

--- lib/day_01/solution.ex
+++ lib/day_01/solution.ex
@@ -1,3 +1,20 @@
-defmodule CodeAdvent2016.Day01.Solution do
+defmodule CodeAdvent2016.Day01.Location do
+  defstruct north: 0,
+            east: 0,
+            direction: :north
+end
+
+defmodule CodeAdvent2016.Day01 do
+  alias CodeAdvent2016.Day01.Location
+
+  def rotate(%Location{direction: :north} = location, "R"), do: %{location | direction: :east}
+  def rotate(%Location{direction: :east} = location, "R"),  do: %{location | direction: :south}
+  def rotate(%Location{direction: :south} = location, "R"), do: %{location | direction: :west}
+  def rotate(%Location{direction: :west} = location, "R"),  do: %{location | direction: :north}
+
+  def rotate(%Location{direction: :north} = location, "L"), do: %{location | direction: :west}
+  def rotate(%Location{direction: :west} = location, "L"),  do: %{location | direction: :south}
+  def rotate(%Location{direction: :south} = location, "L"), do: %{location | direction: :east}
+  def rotate(%Location{direction: :east} = location, "L"),  do: %{location | direction: :north}
 
 end
--- test/day_01/solution_test.exs
+++ test/day_01/solution_test.exs
@@ -2,8 +2,30 @@ defmodule CodeAdvent2016.Day01.SolutionTest do
   use ExUnit.Case
   doctest CodeAdvent2016.Day01
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias CodeAdvent2016.Day01.Location
+
+  test "Rotating right works" do
+    north_facing = %Location{direction: :north}
+    east_facing = %Location{direction: :east}
+    south_facing = %Location{direction: :south}
+    west_facing = %Location{direction: :west}
+
+    assert CodeAdvent2016.Day01.rotate(north_facing, "R") == east_facing
+    assert CodeAdvent2016.Day01.rotate(east_facing, "R") == south_facing
+    assert CodeAdvent2016.Day01.rotate(south_facing, "R") == west_facing
+    assert CodeAdvent2016.Day01.rotate(west_facing, "R") == north_facing
+  end
+
+  test "Rotating left works" do
+    north_facing = %Location{direction: :north}
+    east_facing = %Location{direction: :east}
+    south_facing = %Location{direction: :south}
+    west_facing = %Location{direction: :west}
+
+    assert CodeAdvent2016.Day01.rotate(north_facing, "L") == west_facing
+    assert CodeAdvent2016.Day01.rotate(west_facing, "L") == south_facing
+    assert CodeAdvent2016.Day01.rotate(south_facing, "L") == east_facing
+    assert CodeAdvent2016.Day01.rotate(east_facing, "L") == north_facing
   end
 
 end
