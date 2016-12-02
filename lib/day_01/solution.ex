fca17558f8b87e3814ae8b29892996cf096dc52d
steve.brazier <steve.b@lendinvest.com>
2016-12-02 16:51:55 +0000

    moving

--- lib/day_01/solution.ex
+++ lib/day_01/solution.ex
@@ -2,19 +2,26 @@ defmodule CodeAdvent2016.Day01.Location do
   defstruct north: 0,
             east: 0,
             direction: :north
+
+  def rotate(%__MODULE__{direction: :north} = location, "R"), do: %{location | direction: :east}
+  def rotate(%__MODULE__{direction: :east}  = location, "R"), do: %{location | direction: :south}
+  def rotate(%__MODULE__{direction: :south} = location, "R"), do: %{location | direction: :west}
+  def rotate(%__MODULE__{direction: :west}  = location, "R"), do: %{location | direction: :north}
+
+  def rotate(%__MODULE__{direction: :north} = location, "L"), do: %{location | direction: :west}
+  def rotate(%__MODULE__{direction: :west}  = location, "L"), do: %{location | direction: :south}
+  def rotate(%__MODULE__{direction: :south} = location, "L"), do: %{location | direction: :east}
+  def rotate(%__MODULE__{direction: :east}  = location, "L"), do: %{location | direction: :north}
+
+  def move(%__MODULE__{direction: :north, north: old_value}  = location, change), do: %{location | north: old_value + change}
+  def move(%__MODULE__{direction: :south, north: old_value}  = location, change), do: %{location | north: old_value - change}
+
+  def move(%__MODULE__{direction: :east, east: old_value}  = location, change), do: %{location | east: old_value + change}
+  def move(%__MODULE__{direction: :west, east: old_value}  = location, change), do: %{location | east: old_value - change}
+
 end
 
 defmodule CodeAdvent2016.Day01 do
-  alias CodeAdvent2016.Day01.Location
 
-  def rotate(%Location{direction: :north} = location, "R"), do: %{location | direction: :east}
-  def rotate(%Location{direction: :east} = location, "R"),  do: %{location | direction: :south}
-  def rotate(%Location{direction: :south} = location, "R"), do: %{location | direction: :west}
-  def rotate(%Location{direction: :west} = location, "R"),  do: %{location | direction: :north}
-
-  def rotate(%Location{direction: :north} = location, "L"), do: %{location | direction: :west}
-  def rotate(%Location{direction: :west} = location, "L"),  do: %{location | direction: :south}
-  def rotate(%Location{direction: :south} = location, "L"), do: %{location | direction: :east}
-  def rotate(%Location{direction: :east} = location, "L"),  do: %{location | direction: :north}
 
 end
--- test/day_01/solution_test.exs
+++ test/day_01/solution_test.exs
@@ -10,10 +10,10 @@ defmodule CodeAdvent2016.Day01.SolutionTest do
     south_facing = %Location{direction: :south}
     west_facing = %Location{direction: :west}
 
-    assert CodeAdvent2016.Day01.rotate(north_facing, "R") == east_facing
-    assert CodeAdvent2016.Day01.rotate(east_facing, "R") == south_facing
-    assert CodeAdvent2016.Day01.rotate(south_facing, "R") == west_facing
-    assert CodeAdvent2016.Day01.rotate(west_facing, "R") == north_facing
+    assert Location.rotate(north_facing, "R") == east_facing
+    assert Location.rotate(east_facing, "R") == south_facing
+    assert Location.rotate(south_facing, "R") == west_facing
+    assert Location.rotate(west_facing, "R") == north_facing
   end
 
   test "Rotating left works" do
@@ -22,10 +22,38 @@ defmodule CodeAdvent2016.Day01.SolutionTest do
     south_facing = %Location{direction: :south}
     west_facing = %Location{direction: :west}
 
-    assert CodeAdvent2016.Day01.rotate(north_facing, "L") == west_facing
-    assert CodeAdvent2016.Day01.rotate(west_facing, "L") == south_facing
-    assert CodeAdvent2016.Day01.rotate(south_facing, "L") == east_facing
-    assert CodeAdvent2016.Day01.rotate(east_facing, "L") == north_facing
+    assert Location.rotate(north_facing, "L") == west_facing
+    assert Location.rotate(west_facing, "L") == south_facing
+    assert Location.rotate(south_facing, "L") == east_facing
+    assert Location.rotate(east_facing, "L") == north_facing
+  end
+
+  test "Moving North" do
+    start = %Location{direction: :north, north: 0}
+    five_blocks_north = %Location{direction: :north, north: 5}
+
+    assert Location.move(start, 5) == five_blocks_north
+  end
+
+  test "Moving South" do
+    start = %Location{direction: :south, north: 3}
+    five_blocks_south = %Location{direction: :south, north: -2}
+
+    assert Location.move(start, 5) == five_blocks_south
+  end
+
+  test "Moving East" do
+    start = %Location{direction: :east, east: 0}
+    five_blocks_east = %Location{direction: :east, east: 5}
+
+    assert Location.move(start, 5) == five_blocks_east
+  end
+
+  test "Moving West" do
+    start = %Location{direction: :west, east: 3}
+    five_blocks_west = %Location{direction: :west, east: -2}
+
+    assert Location.move(start, 5) == five_blocks_west
   end
 
 end
