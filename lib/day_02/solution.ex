9ce01aab19a402ef1e3695cc488636a52d11c68f
steve.brazier <steve.b@lendinvest.com>
2016-12-03 13:06:23 +0000

    day 2 part 1

--- lib/day_02/solution.ex
+++ lib/day_02/solution.ex
@@ -1,4 +1,58 @@
+defmodule CodeAdvent2016.Day02.Grid do
+  def key({-1, 1}), do: "1"
+  def key({0, 1}),  do: "2"
+  def key({1, 1}),  do: "3"
+  
+  def key({-1, 0}), do: "4"
+  def key({0, 0}),  do: "5"
+  def key({1, 0}),  do: "6"
+  
+  def key({-1, -1}), do: "7"
+  def key({0, -1}),  do: "8"
+  def key({1, -1}),  do: "9"
+  
+  def move({x, y}, "U"), do: {x, on_grid(y + 1)}
+  def move({x, y}, "D"), do: {x, on_grid(y - 1)}
+  def move({x, y}, "L"), do: {on_grid(x - 1), y}
+  def move({x, y}, "R"), do: {on_grid(x + 1), y}
+
+  defp on_grid(-2), do: -1
+  defp on_grid(2),  do: 1
+  defp on_grid(n),  do: n
+end
+
+defmodule CodeAdvent2016.Day02.Line do
+  alias CodeAdvent2016.Day02.Grid
+
+  def process(line, {x, y}) do
+    end_position = line
+    |> String.graphemes
+    |> Enum.reduce({x, y}, &move/2)
+
+    {Grid.key(end_position), end_position}
+  end
+
+  defp move(direction, {x, y}), do: Grid.move({x, y}, direction)
+end
+
 defmodule CodeAdvent2016.Day02.PartOne do
+  alias CodeAdvent2016.Day02.Line
+  @file_path "lib/day_02/input.txt"
 
+  def run() do
+    {_, key_presses} = @file_path
+    |> File.stream!
+    |> Stream.map(&String.trim/1)
+    |> Enum.reduce({{0, 0}, []}, &process_line/2)
 
+    key_presses
+    |> Enum.reverse
+    |> Enum.join
+    |> IO.inspect
+  end
+
+  defp process_line(line, {{x, y}, key_presses}) do
+    {key, final_position} = Line.process(line, {x, y})
+    {final_position, [key | key_presses]}
+  end
 end
--- test/day_02/solution_test.exs
+++ test/day_02/solution_test.exs
@@ -1,4 +1,48 @@
 defmodule CodeAdvent2016.Day02.SolutionTest do
   use ExUnit.Case
+  alias CodeAdvent2016.Day02.Grid
+  alias CodeAdvent2016.Day02.Line
+
+  test "Each key has co-ords centered on 5" do
+    assert Grid.key({-1, 1}) == "1"
+    assert Grid.key({0, 1})  == "2"
+    assert Grid.key({1, 1})  == "3"
+
+    assert Grid.key({-1, 0}) == "4"
+    assert Grid.key({0, 0})  == "5"
+    assert Grid.key({1, 0})  == "6"
+
+    assert Grid.key({-1, -1}) == "7"
+    assert Grid.key({0, -1})  == "8"
+    assert Grid.key({1, -1})  == "9"
+  end
+
+  test "movement can happen in 4 directions" do
+    assert Grid.move({0, 0}, "U") == {0, 1}
+    assert Grid.move({0, 0}, "D") == {0, -1}
+    assert Grid.move({0, 0}, "L") == {-1, 0}
+    assert Grid.move({0, 0}, "R") == {1, 0}
+  end
+
+  test "movement off grid is ignored" do
+    up_a_lot = {0, 0}
+    |> Grid.move("U")
+    |> Grid.move("U")
+    |> Grid.move("U")
+
+    assert up_a_lot == {0, 1}
+
+    left_a_lot = {0, 0}
+    |> Grid.move("L")
+    |> Grid.move("L")
+    |> Grid.move("L")
+    |> Grid.move("L")
+
+    assert left_a_lot == {-1, 0}
+  end
+
+  test "follow whole line to a key" do
+    assert Line.process("ULL", {0, 0}) == {"1", {-1, 1}}
+  end
 
 end
