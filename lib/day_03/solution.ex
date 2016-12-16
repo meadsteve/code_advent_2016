19cc33548be954d27a6b4a56e469dc418fc35f0e
steve.brazier <steve.b@lendinvest.com>
2016-12-16 08:12:36 +0000

    end to end test for day 3 part 2

--- lib/day_03/solution.ex
+++ lib/day_03/solution.ex
@@ -6,21 +6,57 @@ defmodule CodeAdvent2016.Day03.Triangles do
 
 end
 
+defmodule CodeAdvent2016.Day03.VerticalReader do
+
+  def to_triangles(input) do
+    input
+    |> Stream.chunk(3)
+    |> Stream.flat_map(&triangles_from_columns/1)
+  end
+
+  defp triangles_from_columns([[x1, y1, z1], [x2, y2, z2], [x3, y3, z3]]) do
+  [
+    [x1, x2, x3],
+    [y1, y2, y3],
+    [z1, z2, z3]
+  ]
+  end
+end
+
 defmodule CodeAdvent2016.Day03.PartOne do
   alias CodeAdvent2016.Day03.Triangles
   @file_path "lib/day_03/input.txt"
 
   def run() do
-    @file_path
-    |> File.stream!
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.split(&1, ~r{\s+}, trim: true))
-    |> Stream.map(&to_ints/1)
+    read_in_data
     |> Stream.filter(&Triangles.valid?/1)
     |> Enum.count
+   end
+
+   def read_in_data() do
+     @file_path
+     |> File.stream!
+     |> Stream.map(&String.trim/1)
+     |> Stream.map(&String.split(&1, ~r{\s+}, trim: true))
+     |> Stream.map(&to_ints/1)
    end
 
    defp to_ints(strings) do
      Enum.map(strings, &String.to_integer/1)
    end
+end
+
+defmodule CodeAdvent2016.Day03.PartTwo do
+  alias CodeAdvent2016.Day03.Triangles
+  alias CodeAdvent2016.Day03.VerticalReader
+  alias CodeAdvent2016.Day03.PartOne
+
+
+  def run() do
+    PartOne.read_in_data
+    |> VerticalReader.to_triangles
+    |> Stream.filter(&Triangles.valid?/1)
+    |> Enum.count
+   end
+
 end
--- test/day_03/solution_test.exs
+++ test/day_03/solution_test.exs
@@ -2,6 +2,7 @@ defmodule CodeAdvent2016.Day03.SolutionTest do
   use ExUnit.Case
 
   alias CodeAdvent2016.Day03.Triangles
+  alias CodeAdvent2016.Day03.VerticalReader
 
   test "Valid triangles are valid" do
     assert Triangles.valid?([5, 10, 11])
@@ -12,8 +13,27 @@ defmodule CodeAdvent2016.Day03.SolutionTest do
     refute Triangles.valid?([25, 10, 5])
   end
 
-  test "Complete solution" do
+  test "Complete solution part one" do
     assert CodeAdvent2016.Day03.PartOne.run == 862
+  end
+
+  test "VerticalReader works" do
+    input = [
+      [101, 301, 501],
+      [102, 302, 502],
+      [103, 303, 503]
+    ]
+    expected_triangles = [
+      [101, 102, 103],
+      [301, 302, 303],
+      [501, 502, 503]
+    ]
+
+    assert (input |> VerticalReader.to_triangles |> Enum.take(3)) == expected_triangles
+  end
+
+  test "Complete solution part two" do
+    assert CodeAdvent2016.Day03.PartTwo.run == 1577
   end
 
 end
