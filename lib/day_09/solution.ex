95f71e42c685df24796a8eda8c269bd48f59c3f5
steve.brazier <steve.b@lendinvest.com>
2016-12-22 15:32:30 +0000

    day 09 part 2 soln

--- lib/day_09/solution.ex
+++ lib/day_09/solution.ex
@@ -1,4 +1,4 @@
-defmodule CodeAdvent2016.Day09.Decoder do
+defmodule CodeAdvent2016.Day09.PartOne.Decoder do
 
   def decode(string) do
     string
@@ -40,8 +40,98 @@ defmodule CodeAdvent2016.Day09.Decoder do
   end
 
 end
+
+defmodule CodeAdvent2016.Day09.PartTwo.Decoder do
+
+  def decode(list) when is_list(list) do
+    list
+    |> build([])
+    |> Enum.reverse
+  end
+  def decode(string) do
+    string
+    |> String.graphemes
+    |> decode
+    |> Enum.join
+  end
+
+  defp build([], output), do: output
+  defp build(["(" | rest], output) do
+    {command, after_command} = get_next_command(rest)
+    {new_entries, after_application} = apply_command(command, after_command)
+    build(after_application, [new_entries |output])
+  end
+  defp build([l | rest], output) do
+    build(rest, [l | output])
+  end
+
+  defp get_next_command(rest), do: get_next_command(rest, [])
+  defp get_next_command([")" | rest], command) do
+    [x, y] = command
+    |> Enum.reverse
+    |> Enum.join
+    |> String.split("x")
+    |> Enum.map(&String.to_integer/1)
+
+    {[x, y], rest}
+  end
+  defp get_next_command([command_part | rest], command) do
+    get_next_command(rest, [command_part | command])
+  end
+
+  defp apply_command([x, y], input) do
+    to_repeat = input |> Enum.take(x) |> decode
+    output = 1..y
+    |> Enum.flat_map(fn _ -> to_repeat end)
+    {output, input |> Enum.drop(x)}
+  end
+
+end
+defmodule CodeAdvent2016.Day09.PartTwo.Lengther do
+
+  def decoded_length(list) when is_list(list) do
+    list
+    |> build(0)
+  end
+  def decoded_length(string) do
+    string
+    |> String.graphemes
+    |> decoded_length
+  end
+
+  defp build([], count), do: count
+  defp build(["(" | rest], count) do
+    {command, after_command} = get_next_command(rest)
+    {new_total, after_application} = apply_command(command, after_command)
+    build(after_application, count + new_total)
+  end
+  defp build([l | rest], count) do
+    build(rest, count + 1)
+  end
+
+  defp get_next_command(rest), do: get_next_command(rest, [])
+  defp get_next_command([")" | rest], command) do
+    [x, y] = command
+    |> Enum.reverse
+    |> Enum.join
+    |> String.split("x")
+    |> Enum.map(&String.to_integer/1)
+
+    {[x, y], rest}
+  end
+  defp get_next_command([command_part | rest], command) do
+    get_next_command(rest, [command_part | command])
+  end
+
+  defp apply_command([x, y], input) do
+    count = input |> Enum.take(x) |> decoded_length
+    {count * y, input |> Enum.drop(x)}
+  end
+
+end
+
 defmodule CodeAdvent2016.Day09.PartOne do
-  alias CodeAdvent2016.Day09.Decoder
+  alias CodeAdvent2016.Day09.PartOne.Decoder
   @file_path "lib/day_09/input.txt"
 
   def lines() do
@@ -54,5 +144,16 @@ defmodule CodeAdvent2016.Day09.PartOne do
     |> Decoder.decode
     |> String.trim
     |> String.length
+  end
+end
+
+defmodule CodeAdvent2016.Day09.PartTwo do
+  alias CodeAdvent2016.Day09.PartOne
+  alias CodeAdvent2016.Day09.PartTwo.Lengther
+
+  def run() do
+    PartOne.lines()
+    |> String.trim
+    |> Lengther.decoded_length
   end
 end
--- test/day_09/solution_test.exs
+++ test/day_09/solution_test.exs
@@ -1,30 +1,48 @@
 defmodule CodeAdvent2016.Day09SolutionTest do
   use ExUnit.Case
-  alias CodeAdvent2016.Day09.Decoder
+  alias CodeAdvent2016.Day09.PartOne
+  alias CodeAdvent2016.Day09.PartTwo
 
   test "ADVENT decodes to ADVENT" do
-    assert Decoder.decode("ADVENT") == "ADVENT"
+    assert PartOne.Decoder.decode("ADVENT") == "ADVENT"
   end
 
   test "A(1x5)BC becomes ABBBBBC" do
-    assert Decoder.decode("A(1x5)BC") == "ABBBBBC"
+    assert PartOne.Decoder.decode("A(1x5)BC") == "ABBBBBC"
   end
 
   test "A(2x2)BCD(2x2)EFG becomes ABCBCDEFEFG" do
-    assert Decoder.decode("A(2x2)BCD(2x2)EFG") == "ABCBCDEFEFG"
+    assert PartOne.Decoder.decode("A(2x2)BCD(2x2)EFG") == "ABCBCDEFEFG"
   end
 
   test "(6x1)(1x3)A becomes (1x3)A" do
-    assert Decoder.decode("(6x1)(1x3)A") == "(1x3)A"
+    assert PartOne.Decoder.decode("(6x1)(1x3)A") == "(1x3)A"
   end
 
   test "X(8x2)(3x3)ABCY becomes X(3x3)ABC(3x3)ABCY" do
-    assert Decoder.decode("X(8x2)(3x3)ABCY") == "X(3x3)ABC(3x3)ABCY"
+    assert PartOne.Decoder.decode("X(8x2)(3x3)ABCY") == "X(3x3)ABC(3x3)ABCY"
+  end
+
+  test "X(8x2)(3x3)ABCY becomes XABCABCABCABCABCABCY" do
+    assert PartTwo.Decoder.decode("X(8x2)(3x3)ABCY") == "XABCABCABCABCABCABCY"
+  end
+
+  test "X(8x2)(3x3)ABCY can have its length calculated'" do
+    assert PartTwo.Lengther.decoded_length("X(8x2)(3x3)ABCY") == String.length("XABCABCABCABCABCABCY")
+  end
+
+  test "(27x12)(20x12)(13x14)(7x10)(1x12)A is X long" do
+    assert PartTwo.Lengther.decoded_length("(27x12)(20x12)(13x14)(7x10)(1x12)A") == 241920
   end
 
   @tag :solutions
   test "Part 1 solution" do
-    assert CodeAdvent2016.Day09.PartOne.run == 102239
+    assert PartOne.run == 102239
+  end
+
+  @tag :solutions
+  test "Part 2 solution" do
+    assert PartTwo.run == 102239
   end
 
 end
