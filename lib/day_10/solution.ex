6fdea1f85a84249a6b6326941c394ac966dce6af
steve.brazier <steve.b@lendinvest.com>
2017-01-07 12:57:56 +0000

    registry and bot supervisor

--- lib/day_10/solution.ex
+++ lib/day_10/solution.ex
@@ -1,32 +1,32 @@
 defmodule CodeAdvent2016.Day10.Bot do
   use GenServer
 
-  def start_link! do
-    {:ok, bot} = start_link(self())
-    bot
+  def start_link!(bot_number) do
+    {:ok, bot} = start_link(bot_number)
+    bot_number
   end
 
-  def start_link(owner) do
-    GenServer.start_link(__MODULE__, %{:owner => owner})
+  def start_link(bot_number) do
+    GenServer.start_link(__MODULE__, %{}, name: via_tuple(bot_number))
   end
 
-  def give_chip(bot, value) do
-    GenServer.cast(bot, {:give_chip, value})
-    bot
+  def give_chip(bot_number, value) do
+    GenServer.cast(via_tuple(bot_number), {:give_chip, value})
+    bot_number
   end
 
-  def get_chips(bot) do
-    GenServer.call(bot, :get_chips)
+  def get_chips(bot_number) do
+    GenServer.call(via_tuple(bot_number), :get_chips)
   end
 
-  def set_low_target(bot, bot: bot_number) do
-    GenServer.cast(bot, {:set_low_target, bot_number})
-    bot
+  def set_low_target(bot_number, target: target) do
+    GenServer.cast(via_tuple(bot_number), {:set_low_target, target})
+    bot_number
   end
 
-  def set_high_target(bot, bot: bot_number) do
-    GenServer.cast(bot, {:set_high_target, bot_number})
-    bot
+  def set_high_target(bot_number, target: target) do
+    GenServer.cast(via_tuple(bot_number), {:set_high_target, target})
+    bot_number
   end
 
   # SERVER
@@ -38,7 +38,7 @@ defmodule CodeAdvent2016.Day10.Bot do
   def handle_cast({:give_chip, value}, bot_state) do
     updated_state = Map.update(bot_state, :values, [value], fn [other] -> [other, value] end)
     case Map.get(updated_state, :values) do
-      [a, b] -> send_values(bot_state, [a, b])
+      [a, b] -> send_chips(bot_state, [a, b])
       _      -> :do_nothing
     end
     {:noreply, updated_state}
@@ -56,14 +56,92 @@ defmodule CodeAdvent2016.Day10.Bot do
     {:reply, Map.get(bot_state, :values, []), bot_state}
   end
 
-  defp send_values(bot_data, [a, b]) do
-    send Map.get(bot_data, :owner),
-         {:send, to_bot: Map.get(bot_data, :low), value: min(a, b)}
-    send Map.get(bot_data, :owner),
-         {:send, to_bot: Map.get(bot_data, :high), value: max(a, b)}
+  defp send_chips(bot_data, [a, b]) do
+    give_chip(Map.get(bot_data, :low), min(a, b))
+    give_chip(Map.get(bot_data, :high), max(a, b))
   end
 
+  defp via_tuple(bot_number) do
+    {:via, CodeAdvent2016.Day10.BotRegistry, {:bot_number, bot_number}}
+  end
+
+
 end
+
+defmodule CodeAdvent2016.Day10.BotSupervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: :bot_supervisor)
+  end
+
+  def start_bot!(bot_number) do
+    {:ok, bot} = Supervisor.start_child(:bot_supervisor, [bot_number])
+    bot
+  end
+
+  def init(_) do
+    children = [
+      worker(CodeAdvent2016.Day10.Bot, [])
+    ]
+    supervise(children, strategy: :simple_one_for_one)
+  end
+end
+
+defmodule CodeAdvent2016.Day10.BotRegistry do
+  use GenServer
+
+  # API
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def whereis_name(bot_number) do
+    GenServer.call(__MODULE__, {:whereis_bot, bot_number})
+  end
+
+  def register_name(bot_number, pid) do
+    GenServer.call(__MODULE__, {:register_bot, bot_number, pid})
+  end
+
+  def unregister_name(bot_number) do
+    GenServer.cast(__MODULE__, {:unregister_bot, bot_number})
+  end
+
+  def send(bot_number, message) do
+    case whereis_name(bot_number) do
+      :undefined ->
+        {:badarg, {bot_number, message}}
+      pid ->
+        Kernel.send(pid, message)
+        pid
+    end
+  end
+
+  # SERVER
+  def init(_) do
+    {:ok, Map.new}
+  end
+
+  def handle_call({:whereis_bot, bot_number}, _from, state) do
+    {:reply, Map.get(state, bot_number, :undefined), state}
+  end
+
+  def handle_call({:register_bot, bot_number, pid}, _from, state) do
+    case Map.get(state, bot_number) do
+      nil ->
+        {:reply, :yes, Map.put(state, bot_number, pid)}
+      _ ->
+        {:reply, :no, state}
+    end
+  end
+
+  def handle_cast({:unregister_bot, bot_number}, state) do
+    {:noreply, Map.delete(state, bot_number)}
+  end
+end
+
 
 defmodule CodeAdvent2016.Day10.PartOne do
   @file_path "lib/day_10/input.txt"
--- test/day_10/solution_test.exs
+++ test/day_10/solution_test.exs
@@ -1,46 +1,67 @@
-defmodule CodeAdvent2016.Day10SolutionTest do
+defmodule CodeAdvent2016.Day10PartsTest do
   use ExUnit.Case
-  alias CodeAdvent2016.Day10.PartOne
   alias CodeAdvent2016.Day10.Bot
+  alias CodeAdvent2016.Day10.BotSupervisor
+  alias CodeAdvent2016.Day10.BotRegistry
+
+  setup do
+    {:ok, _bot_registry} = BotRegistry.start_link
+    :ok
+  end
 
   test "Bot starts with [] chips" do
-    bot = Bot.start_link!
+    bot = Bot.start_link!(1)
     assert Bot.get_chips(bot) == []
   end
 
   test "Bot takes 1 chip" do
-    bot = Bot.start_link!
+    bot = Bot.start_link!(1)
     |> Bot.give_chip(5)
 
     assert Bot.get_chips(bot) == [5]
   end
 
-  test "Bot takes 2 chips and sends them away so it's left with []'" do
-    bot = Bot.start_link!
+  test "Bot messages 2 to give its low chip to the set target" do
+    Bot.start_link!(2)
+
+    Bot.start_link!(1)
+    |> Bot.set_low_target(target: 2)
     |> Bot.give_chip(5)
     |> Bot.give_chip(3)
 
-    assert Bot.get_chips(bot) == [5, 3]
-  end
-
-  test "Bot messages us to give its low chip to the set target" do
-    Bot.start_link!
-    |> Bot.set_low_target(bot: 2)
-    |> Bot.give_chip(5)
-    |> Bot.give_chip(3)
-
-    assert_receive {:send, to_bot: 2, value: 3}
+    Process.sleep 100
+    assert Bot.get_chips(2) == [3]
   end
 
   test "Bot messages us to give its high chip to the set target" do
-    Bot.start_link!
-    |> Bot.set_high_target(bot: 10)
+    Bot.start_link!(10)
+
+    Bot.start_link!(1)
+    |> Bot.set_high_target(target: 10)
     |> Bot.give_chip(5)
     |> Bot.give_chip(3)
 
-    assert_receive {:send, to_bot: 10, value: 5}
+    Process.sleep 100
+    assert Bot.get_chips(10) == [5]
   end
 
+  test "Bots can be started by a supervisor" do
+    {:ok, _} = BotSupervisor.start_link()
+
+    BotSupervisor.start_bot!(1)
+    BotSupervisor.start_bot!(2)
+
+    Bot.give_chip(1, 5)
+    Bot.give_chip(2, 3)
+
+    assert Bot.get_chips(1) == [5]
+    assert Bot.get_chips(2) == [3]
+  end
+end
+
+defmodule CodeAdvent2016.Day10SolutionTest do
+  use ExUnit.Case
+  alias CodeAdvent2016.Day10.PartOne
   @tag :solutions
   test "Part 1 solution" do
     assert PartOne.run == ""
